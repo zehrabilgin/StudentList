bafb7da19db9bc8c7588ee767e832e3743410bd5
Zehra <zehrabilgin341@gmail.com>
2021-01-26 14:50:14 +0300

    Student-StudentList Homework

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new StudentList();
+
+            list.Add(new Student { Id = 1 , Name = "Nikolay", Surname = "Gogol",GPA= 2.50});
+            list.Add(new Student { Id = 2, Name = "Fyodor", Surname = "Dostoevsky", GPA = 3.48 });
+            list.Add(new Student { Id = 3, Name = "Leo", Surname = "Tolstoy", GPA = 3.45 });
+            list.Add(new Student { Id = 4, Name = "Alexander", Surname = "Pushkin", GPA = 3.71 });
+            list.Add(new Student { Id = 4, Name = "Ernest", Surname = "Hemingway", GPA = 3.11 });
+
+
+            var highestGpas = list.GetStudentWithHighestGPA();
+            highestGpas.ForEach(s => Console.WriteLine($"Soyad: {s.Surname} ve GPA: {s.GPA}"));
+
+            var results = list.GetStudentByName("Fyodor");
+            results.ForEach(s => Console.WriteLine($"{s.Surname}"));
+
+            list.RemoveBySurname("Hemingway");
+
+            list.Yazdır();
+
+            var getIdByUser = list.GetStudentById(4);
+            Console.WriteLine(getIdByUser.Name + getIdByUser.Surname);
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ StudentList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentList
+{
+    class StudentList
+    {
+        private List<Student> students = new List<Student>();
+        public void Add(Student student)
+        {
+            students.Add(student);
+        }
+
+        public Student GetStudentById(int id)
+        {
+            return students.FirstOrDefault(s => s.Id == id);
+        }
+
+        public List<Student> GetStudentByName(string name)
+        {
+            return students.Where(s => s.Name.Contains(name)).ToList();
+        }
+
+        public void Yazdır()
+        {
+            students.ForEach(i => Console.WriteLine(i.Name));
+        }
+
+        public void RemoveBySurname(string surname)
+        {
+            students.Remove(students.FirstOrDefault(s => s.Surname.Equals(surname)));
+        }
+
+        public List<Student> GetStudentWithHighestGPA()
+        {
+            return students.Where(x => x.GPA == students.Max(y => y.GPA)).ToList();
+        }
+
+
+    }
+}
